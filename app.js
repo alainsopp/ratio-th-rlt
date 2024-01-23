1484b8786c9a2c6c1178733a195b23d8a5ffc370
kutor <n.catin@protonmail.com>
2024-01-23 22:19:09 +0100

    Fix issue about all the mines not being analyzed

--- app.js
+++ app.js
@@ -6,35 +6,35 @@ function log_event(msg) {
 }
 
 function addRatio() {
-    try {        
-        cards = document.getElementsByClassName('marketplace-buy-item-card')        
-        for (i = 0; i < cards.length; i++) {
-            var imageTag = cards[i].getElementsByTagName('img')            
-            if (imageTag[0].src.includes('miners')) {                                   
+    try {
+        cards = document.getElementsByClassName('marketplace-buy-item-card')
+        for ( i = 0; i < cards.length; i++ ) {            
+            if ( cards[i].href.includes('miner') ) {
                 price = parseFloat(cards[i].getElementsByClassName('item-price')[0].textContent)
                 power = cards[i].getElementsByClassName('item-addition-power')[0].textContent.split(' ')
                 mul = { 'Gh/s': 1, 'Th/s': 1000, 'Ph/s': 1000000 }
                 unit = mul[power[2]]
                 power = parseFloat(power[0]) * unit
                 ratio = (power / price) / 1000
-                
-                var priceWrapper = cards[i].getElementsByClassName('item-price-wrapper')[0]       
+
+                var priceWrapper = cards[i].getElementsByClassName('item-price-wrapper')[0]
                 var ratioTitleNode = document.createElement('p')
                 ratioTitleNode.classList.add('item-price-label')
                 var textNode = document.createTextNode('Ratio')
                 ratioTitleNode.appendChild(textNode)
                 priceWrapper.appendChild(ratioTitleNode)
-                
+
                 ratioValueNode = document.createElement('p')
                 ratioValueNode.classList.add('item-price')
                 ratioValueNode.style.color = "lightgreen"
                 textNode = document.createTextNode(ratio.toFixed(3))
                 ratioValueNode.appendChild(textNode)
                 priceWrapper.appendChild(ratioValueNode)
-            }            
-        }        
+            }
+        }
     } catch (error) {
         log_event(error)
     }
 }
-addRatio()
+
+setTimeout(() => addRatio(), 5000)
